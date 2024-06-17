912caad03feef924338b09905174923a75a28703
ibrhmhnfm <130213828+ibrhmhnfm@users.noreply.github.com>
2024-06-17 16:46:44 +0700

    Update fungsi2.h

--- algoritma/fungsi2.h
+++ algoritma/fungsi2.h
@@ -1,19 +1,23 @@
 #ifndef FUNGSI2_H
 #define FUNGSI2_H
 
-#define MAX 100
-#define SHORT 20
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "const.h"
+#define SHORT 20
+#define MAX 100
 
 // Function prototypes
-void tambah_riwayat(RiwayatPasien *head, BiayaTindakan *head2, Pasien *head3, int total);
-void edit_riwayat(RiwayatPasien *head);
-void hapus_riwayat(RiwayatPasien *head);
+void simpan_csv_riwayat(const char *nama_file, RiwayatPasien *head);
 void cari_riwayat(RiwayatPasien *head);
+int last_index_riwayat(RiwayatPasien *head);
+int cek_id(char *tes, Pasien *head);
+int cek_tindakan(char *tes, BiayaTindakan *head);
+double hitung_tindakan(char *tes, BiayaTindakan *head);
+void clear_input_buffer();
+void tambah_riwayat(RiwayatPasien **head, Pasien *head3, BiayaTindakan *head2);
+void edit_riwayat(RiwayatPasien *head, Pasien *pasien, BiayaTindakan *harga);
+void hapus_riwayat(RiwayatPasien *head, Pasien *pasien);
 
-int count_riwayat(RiwayatPasien *head);
-int count_pasien(Pasien *head);
-int count_tindakan(BiayaTindakan *head);
-
-#endif // FUNGSI2_H
+#endif /* FUNGSI2_H */
